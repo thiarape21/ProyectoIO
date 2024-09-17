f357bf4af0a0e55c7bb45f5f2439eb65d8c43b8c
IssaAC00 <paola.alguera@estudiantec.cr>
2024-09-17 11:53:01 -0600

    cambio casi completo
    cambia la matriz donde tiene que haber un uno pero se bugea cuando cambia las filas a cero

--- simplex-app/src/components/SimplexAlgorithm.js
+++ simplex-app/src/components/SimplexAlgorithm.js
@@ -14,7 +14,7 @@ function simplexBasic(vari, res) { //arma la matrix segun la cantidad de restric
             matrix[i] = [];
             for (let j = 0; j < colums; j++) {
                 if (j === 0) {
-                    matrix[i][j] = i - 1;  
+                    matrix[i][j] = i - 1;
                 } else if (i === 1 && j === 1) {
                     matrix[1][1] = "z";
                 } else if (j === 1 && i > 1) {
@@ -53,9 +53,9 @@ function construirArray(vari, res) { // hace el encabezado de la matrix
 function llenarSistemaEnMatriz(matriz, sistema) { // tiene que entrarle un sistema de ecuaciones como el de abajo para poder remplazarlo en la matriz que se contruyo
 
     for (let i = 0; i < sistema.length; i++) {
-        let ecuacion = sistema[i]; 
+        let ecuacion = sistema[i];
         for (let j = 2; j < matriz[i + 1].length - 1; j++) {
-            matriz[i + 1][j] = ecuacion[j - 2] || 0; 
+            matriz[i + 1][j] = ecuacion[j - 2] || 0;
         }
         matriz[i + 1][matriz[i + 1].length - 1] = ecuacion[ecuacion.length - 1];
     }
@@ -66,16 +66,16 @@ function llenarSistemaEnMatriz(matriz, sistema) { // tiene que entrarle un siste
 
 
 function encontrarIndiceMenorValorFilaZ(matriz) {
-    let filaZ = matriz[1]; 
-    let valores = filaZ.slice(2, -1); 
+    let filaZ = matriz[1];
+    let valores = filaZ.slice(2, -1);
     let menorValor = Math.min(...valores.filter(valor => typeof valor === 'number'));
-    if (menorValor < 0){
+    if (menorValor < 0) {
         let indiceMenorValor = valores.indexOf(menorValor);
-        return indiceMenorValor + 2 ;// ajuste del tamaño real de la matriz
+        return indiceMenorValor + 2;// ajuste del tamaño real de la matriz
     }
-   else{
+    else {
         return -1;
-   }
+    }
 }
 
 
@@ -83,20 +83,20 @@ function encontrarIndiceMenorValorFilaZ(matriz) {
 // Función para calcular los valores en la columna de 'Radios'
 function calcularRadios(matriz) {
     let columnaIndiceMenor = encontrarIndiceMenorValorFilaZ(matriz);
-    let indiceColumnaRHS= matriz[1].length;
-    
-    for (let i = 1; i < matriz.length; i++) { 
+    let indiceColumnaRHS = matriz[1].length;
+
+    for (let i = 1; i < matriz.length; i++) {
         let valorColumna = matriz[i][columnaIndiceMenor];
-        let rhs = matriz[i][indiceColumnaRHS-2]; 
+        let rhs = matriz[i][indiceColumnaRHS - 2];
 
         if (valorColumna === 0) {
-            matriz[i][indiceColumnaRHS-1] = '+INF'; 
+            matriz[i][indiceColumnaRHS - 1] = '+INF';
         } else if ((rhs < 0 && valorColumna > 0) || (rhs > 0 && valorColumna < 0)) {
-            matriz[i][indiceColumnaRHS-1] = '+INF'; 
+            matriz[i][indiceColumnaRHS - 1] = '+INF';
         } else if (rhs === 0) {
-            matriz[i][indiceColumnaRHS-1] = '+INF'; 
+            matriz[i][indiceColumnaRHS - 1] = '+INF';
         } else {
-            matriz[i][indiceColumnaRHS-1] = rhs / valorColumna; 
+            matriz[i][indiceColumnaRHS - 1] = rhs / valorColumna;
         }
     }
 
@@ -106,14 +106,14 @@ function calcularRadios(matriz) {
 
 
 function encontrarIndiceColumnaMenorRadios(matriz) {
-   
+
     const indiceColumnaRadio = matriz[0].indexOf('Radios');
     if (indiceColumnaRadio === -1) {
         throw new Error("'Radios' no se encuentra en la matriz");
     }
-    
+
     let radios = matriz.slice(1).map(fila => fila[indiceColumnaRadio]);
-    
+
     let valoresRadios = radios.map(valor => {
         if (valor === 'N/A' || valor === '+INF') {
             return Infinity; // Considerar '+INF' como infinito
@@ -124,34 +124,33 @@ function encontrarIndiceColumnaMenorRadios(matriz) {
     let menorValor = Math.min(...valoresRadios);
 
     let indiceMenorValor = valoresRadios.indexOf(menorValor);
-    return indiceMenorValor + 1; 
+    return indiceMenorValor + 1;
 }
 
 
 
 
 
-function convertirfila1(matrix){
+function convertirfila1(matrix) {
 
     let columna = encontrarIndiceMenorValorFilaZ(matrix);
     let fila = encontrarIndiceColumnaMenorRadios(matrix);
+    
 
     if (fila === undefined || columna === undefined || fila < 0 || columna < 0) {
         throw new Error("Índice de fila o columna inválido.");
     }
 
+    let sub = matrix[fila][columna]; // por el que hay que dividir
+    console.log('divisor: ' + sub);
 
-    console.log( "fila: " + fila + " columna: " + columna);
-    let sub = matrix[fila][columna];
-    console.log(sub);
+    let linea = matrix[fila].slice(2, -1); //escoge la porcion de matriz que realizar
 
-    let linea = matrix[fila].slice(2,-1);
-    console.log(linea);
 
-    if (sub === 1){
+    if (sub === 1) {
         return matrix;
     }
-    else{
+    else {
         let nuevaLinea = linea.map(valor => {
             if (valor === '+INF' || valor === 'N/A') {
                 return valor; // No dividir valores especiales
@@ -164,55 +163,75 @@ function convertirfila1(matrix){
 
         matrix[fila].splice(2, linea.length, ...nuevaLinea);
 
+        console.log("matriz despues de poner un 1: ");
+        console.log(matrix);
         return matrix;
     }
 
 }
 
+function convertirColumnas0(matriz, filaConUno) {
+    let columna = encontrarIndiceMenorValorFilaZ(matriz);// encuentra el valor de la columna 
 
-function convertirColumnas0(matriz){
-    let columna = encontrarIndiceMenorValorFilaZ(matriz);
-    let fila1 = encontrarIndiceColumnaMenorRadios(matriz);
-
-    if (fila1 === undefined || columna === undefined || fila1 < 0 || columna< 0) {
-        throw new Error("Índice de fila o columna inválido.");
+    if (columna === undefined || columna < 0) {
+        throw new Error("Índice de columna inválido."); // espera valor valido de columna 
     }
 
+    for (let i = 1; i < matriz.length; i++) { 
+        if (i === filaConUno) {
+            continue; // ignora la fila con el 1
+        }
 
-    while (columna != -1){
+        let valorColumna = matriz[i][columna];  //empieza en uno por el encabezado se supone que agarra fila de i y columna que seria j 
 
-        columna = encontrarIndiceMenorValorFilaZ(matriz);
+        console.log('deberia agarrar el valor : ' + valorColumna);
+        if (valorColumna === 0) {
+            continue; // ignora si hay un 0 en esa posicion 
+        } else {
+           
+            let nuevaFila = matriz[i].slice(2, -1).map((valor) => {
+                if (typeof valor === 'number') {
+                    console.log('numero de valor que entra:' + valor);
+                    return  ((-1 * valor) * filaConUno[i]  + valorColumna);
+                }
+                return valor;
+            });
 
-
-
+            matriz[i].splice(2, nuevaFila.length, ...nuevaFila);
+        }
     }
-    
 
-
+    return matriz;
 }
 
 
-export function casoBase(){
+export function casoBase() {
+
+    let matrix1 = simplexBasic(2, 2);
+
+    // si el sistema entra asi no necesito llenar la matriz 
+
+    let sistema = [
+        [-2, -1, 0, 0, 0 ,'N/A'], // -2x1 - 1x2 + 0s3 + 0s4 = 0 radios 0
+        [4, 1, 1 , 0, 3],   // 4x1 + 1x2 + 1s3 + 0s4 = 3 radios 0
+        [4, 1, 0, 1, 2]    // 4x1 + 1x2 + 0s3 + 1s4 = 2 radios 0
+    ];
+
+    let matriz = llenarSistemaEnMatriz(matrix1, sistema);
+
+    let matrix = calcularRadios(matriz);
     
-let matrix1= simplexBasic(2,2);
+    let fila1 = encontrarIndiceColumnaMenorRadios(matriz);
 
-// si el sistema entra asi no necesito llenar la matriz 
+    let linea = matriz[fila1].slice(2, -1);
 
-let sistema = [
-    [1, -1, -2, 0, 0, 'N/A'], // -2x1 - 1x2 + 0s3 + 0s4 = 0 radios 0
-    [4, 1, 3, 0, 3, 0],   // 4x1 + 1x2 + 1s3 + 0s4 = 3 radios 0
-    [4, 1, 0, 1, 2, 0]    // 4x1 + 1x2 + 0s3 + 1s4 = 2 radios 0
-];
+    let iteracion1 = convertirfila1(matrix);
 
-let matriz = llenarSistemaEnMatriz(matrix1,sistema);
-
-let matrix = calcularRadios(matriz);
-
-let iteracion1= convertirfila1(matrix);
+    //iteracion1 = convertirColumnas0(iteracion1, linea);
 
 
 
-console.log(iteracion1);
+    console.log(iteracion1);
 
 
 
