86914fd961000b3999b6d54d7783fde84b69dc3a
IssaAC00 <paola.alguera@estudiantec.cr>
2024-10-13 23:52:48 -0600

    updatelast

--- simplex-app/src/Algorithms/simplex_casoBase.js
+++ simplex-app/src/Algorithms/simplex_casoBase.js
@@ -13,10 +13,10 @@ import { encogerMatriz } from '../Algorithms/simplex_dosFases';
 export function simplexBasic(vari, res, arti, metodo, holgura) {//! aqui tiene que ser armar la matrix con el gran M
     const matrix = [];
     const rows = (metodo.includes('Caso Base') || metodo.includes('Gran M')) ? res + 2 : res + 3;
-   // console.log(metodo);
-  //  console.log(`las filas es : ${rows}`);
+    // console.log(metodo);
+    //  console.log(`las filas es : ${rows}`);
     const colums = vari + holgura + arti + 4; //! mas 4 si es dos fases 
- //   console.log(`las columnas es: ${colums}`);
+    //   console.log(`las columnas es: ${colums}`);
     matrix[0] = construirArray(vari, res, arti, holgura);
     const prueba = (arti > 0 && metodo.includes('Dos Fases')) ? 2 : 1;
     const as = (arti > 0 && metodo.includes('Dos Fases')) ? 3 : 2;
@@ -172,19 +172,23 @@ export function calcularRadios(matriz, arti, metodo) {
 
 
 
-  //  console.log(`fila inicio ${filaInicio}`);
+    //  console.log(`fila inicio ${filaInicio}`);
 
     for (let i = filaInicio; i < matriz.length; i++) {
         const valorColumna = matriz[i][columnaIndiceMenor];
         const rhs = matriz[i][indiceColumnaRHS];
         if (valorColumna === 0 || rhs < 0) {
             matriz[i][indiceResultado] = '+INF';
-        } else {
+        }
+        else if (valorColumna < 0) {
+            matriz[i][indiceResultado] = '+INF';
+        }
+        else {
             matriz[i][indiceResultado] = rhs / valorColumna;
         }
     }
-  //  console.log('calculo de radios');
-   // console.log(matriz);
+    //  console.log('calculo de radios');
+    // console.log(matriz);
     return matriz;
 }
 
@@ -212,12 +216,12 @@ export function encontrarIndiceColumnaMenorRadios(matriz, arti, metodo) {
 
     if (arti && metodo.trim() === 'Dos Fases') {
         radios = matriz.slice(2).map(fila => fila[indiceColumnaRadio]);
-       // console.log('asi se ven los radios con dos fases');
+        // console.log('asi se ven los radios con dos fases');
     } else {
         radios = matriz.slice(1).map(fila => fila[indiceColumnaRadio]);
-       // console.log('asi se ven los radios con 1 fase');
+        // console.log('asi se ven los radios con 1 fase');
     }
-    
+
 
     let valoresRadios = radios.map(valor => {
         if (valor === 'N/A' || valor === '+INF') {
@@ -286,7 +290,7 @@ export function convertirfila1(matrix, arti) {
 
     let columna = encontrarIndiceMenorValorFilaZ(matrix, arti);
     let fila = encontrarIndiceColumnaMenorRadios(matrix, arti, 'Caso Base');
-//    console.log(`fila que entra ${fila}`)
+    //    console.log(`fila que entra ${fila}`)
     let variableEntrante = matrix[0][columna];
     // let variableSaliente = matrix[fila][1];
 
@@ -381,7 +385,7 @@ export function casoBase(variable, res, sistema, arti, holgura) {
     if (arti === 0) {
         let matrix1 = simplexBasic(variable, res, 0, 'Caso Base', holgura);
         matriz = llenarSistemaEnMatriz(matrix1, sistema, holgura);
-      //  console.log(matriz);
+        //  console.log(matriz);
     }
 
     else {
--- simplex-app/src/components/SimplexForm.js
+++ simplex-app/src/components/SimplexForm.js
@@ -16,7 +16,7 @@ function SimplexForm() {
   );
 
   const [restrictionOperators, setRestrictionOperators] = useState(
-    Array(parseInt(restrictions)).fill(method === 'General' ? '≥' : '≥')
+    Array(parseInt(restrictions)).fill(method === 'General' ? '≤' : '≤')
   );
 
   const [errorMessage, setErrorMessage] = useState('');
@@ -80,14 +80,14 @@ function SimplexForm() {
           parseInt(contarholgura()));
         console.log(resultado);
 
-         if (resultado.devuelvo === 'no factible') {
+        if (resultado.devuelvo === 'no factible') {
           matrix = resultado.iteraciones;
          // navigate('/data', { state: { matrix } });
         } else {
           matrix = casoBase(parseInt(variables), parseInt(restrictions), resultado.iteraciones, parseInt(contarArtificiales()));
           let iteraciones = resultado.iteraciones;
         //  navigate('/data', { state: { resultado, matrix } });
-        } 
+        }
       } else if (method === 'Gran M') {
         const sistema = convertGranM();
         resultado = granM(sistema, parseInt(variables), parseInt(restrictions),
@@ -102,8 +102,8 @@ function SimplexForm() {
         setErrorMessage('Método no reconocido.');
         return;
       }
-  
-    //  navigate('/data', { state: { resultado, matrix } });
+
+      navigate('/data', { state: { resultado, matrix } });
 
     }
   };
@@ -139,7 +139,7 @@ function SimplexForm() {
   };
 
   const contarArtificiales = () => {
-    return restrictionOperators.filter(elem => elem === "=" || elem === "≥").length;
+    return restrictionOperators.filter(elem => elem === "=" || elem === "≤").length;
   };
 
   const contarholgura = () => {
@@ -155,7 +155,12 @@ function SimplexForm() {
     const matrix = [];
     const empezar = (parseInt(contarArtificiales()) > 0 ? 1 : 0);
 
-
+    console.log(objectiveValues);
+    console.log(restrictionsValues);
+    console.log(`Cantidad de columnas: ${columna}`);
+    console.log(`Cantidad de filas: ${filas}`);
+    console.log(`Cantidad de artificiales: ${parseInt(contarArtificiales())}`);
+    console.log(`Cantidad de holgura: ${contarholgura()}`);
 
     if (parseInt(contarArtificiales()) !== 0) {
       const w = Array.from({ length: columna }, (_, k) => (k >= arti && k < columna - 1 ? 1 : 0));
@@ -174,14 +179,14 @@ function SimplexForm() {
         } else {
           if (i > empezar && j >= variables) {
             matrix[i][j] = 0;
-            if (restrictionOperators[i - 2] === "≤") {
+            if (restrictionOperators[i - 2] === "≥") {
               matrix[i][variables1 - 1 + i - 1] = 1;
               if (j === columna - 1) {
                 matrix[i][j] = restrictionsValues[i - 2][restrictionsValues[0].length - 1];
               }
-            } else if (restrictionOperators[i - 2] === "≥") {
+            } else if (restrictionOperators[i - 2] === "≤") {
               matrix[i][variables1 - 1 + i - 1] = -1;
-              matrix[i][(variables1 + holgura - 1) + i - 1] = 1;//!este es el que acada rato se cambia 
+              matrix[i][(variables1 + holgura - 1) + i - 2] = 1;
               if (j === columna - 1) {
                 matrix[i][j] = restrictionsValues[i - 2][restrictionsValues[0].length - 1];
               }
@@ -197,6 +202,7 @@ function SimplexForm() {
         }
       }
     }
+    console.log(matrix);
     return matrix;
   };
 
@@ -235,12 +241,12 @@ function SimplexForm() {
           // Filas de restricciones
           if (i > 0 && j >= variables) {
             matrix[i][j] = 0;
-            if (restrictionOperators[i - 1] === "≤") {
+            if (restrictionOperators[i - 1] === "≥") {
               matrix[i][variables1 - 1 + i] = 1;
               if (j === columna - 1) { // RHS
                 matrix[i][j] = restrictionsValues[i - 1][restrictionsValues[0].length - 1];
               }
-            } else if (restrictionOperators[i - 1] === "≥") {
+            } else if (restrictionOperators[i - 1] === "≤") {
               matrix[i][variables1 - 1 + i - 1] = -1;
               matrix[i][(variables1 + holgura - 1) + i - 1] = 1;
               if (j === columna - 1) { // RHS
@@ -305,7 +311,7 @@ function SimplexForm() {
               >
                 {method === 'casobase' ? (
                   // Solo muestra ≥ cuando el método es 'casobase'
-                  <option value="≥">≥</option>
+                  <option value="≤">≤</option>
                 ) : (
                   <>
                     <option value="≥">≥</option>
